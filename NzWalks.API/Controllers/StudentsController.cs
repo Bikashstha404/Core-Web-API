4aebb7cba49b882c668eb77eaf4fc680fab03a49
Bikash Shrestha <stha.bikam99@gmail.com>
2024-06-02 14:39:48 +0545

    Created a new controller-StudentsController

---
+++ NzWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NzWalks.API.Controllers
+{
+    //http://localhost:portnumber/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        //Get:
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "Bikash", "Bikram", "Bigyan", "Rohit", "Rajan" };
+
+            return Ok(studentNames);
+        }
+    }
+}
